5221d22147bbeb172aa4da718e5b0d2002bbb494
Prasanga Pokharel <138413854+prasangapokharel@users.noreply.github.com>
2024-01-16 15:28:56 +0545

    Update 2x2 Matrix.c

--- 2x2 Matrix.c
+++ 2x2 Matrix.c
@@ -2,18 +2,21 @@
 
 int main()
 {
-    // matrix 2x2 3x3
+    // Declare a 2x2 matrix
     int arr[2][2];
 
+    // Input values for the matrix elements
     for (int i = 0; i < 2; i++)  
     {
         for (int j = 0; j < 2; j++) 
         {
-            printf("Enter value in a[%d][%d] ", i,j);
+            printf("Enter value in arr[%d][%d]: ", i, j);
             scanf("%d", &arr[i][j]);    
         }
     }
 
+    // Display the entered matrix
+    printf("Entered Matrix:\n");
     for (int i = 0; i < 2; i++)  
     {
         for (int j = 0; j < 2; j++) 
@@ -22,9 +25,6 @@ int main()
         }
         printf("\n");
     }
-
-
-    
 
     return 0;
 }
